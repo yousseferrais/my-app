788f2b2f495275e722cb4257e1355dbccc38db5a
Adem Elabed <adamelaabed@gmail.com>
2022-08-29 17:50:09 +0100

    feat: add registration,service selection and sign-in pages

---
+++ src/routes/registration/+page.svelte
@@ -0,0 +1,39 @@
+<form class="flex flex-col  " action="#">
+  <div>
+    <label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname" required />
+  </div>
+
+  <div>
+    <label for="lname">Last name:</label>
+    <input type="text" id="lname" name="lname" required />
+  </div>
+  <div>
+    <label for="email">E-mail</label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      placeholder="flen@gmail.com"
+      required
+    />
+  </div>
+  <div>
+    <label for="mot">Mot de passe</label>
+    <input type="password" id="mot" name="mot" required />
+  </div>
+  <div>
+    <label for="confirmer">Confirmer le mot de passe</label>
+    <input type="password" id="confirmer" name="confirmer" required />
+  </div>
+  <div>
+    <label for="phone">Numéro de téléphone</label>
+    <input type="text" id="phone" name="phone" pattern="[0-9]{8}" required />
+  </div>
+  <div>
+    <label for="location">Adresse</label>
+    <input type="text" id="location" name="location" required />
+  </div>
+
+  <input type="submit" value="Submit" />
+</form>
---
+++ src/routes/service-selection/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  let budget = 50;
+</script>
+
+<form class="flex flex-col w-max mx-auto" action="#">
+  <div>
+    <label for="">Sélectionner le service désiré : </label>
+    <select name="service" id="service">
+      <option value="" disabled selected hidden />
+      <option value="plumbing">Plomberie</option>
+      <option value="gardening">Jardinage</option>
+      <option value="electricity">Electricité</option>
+      <option value="baby-sitting">Baby-sitting</option>
+      <option value="housework">Ménage</option>
+
+      <option value="private lesson">Cours particulier</option>
+    </select>
+  </div>
+  <div>
+    <label for="budget">Indiquer votre budget : </label>
+    <input type="range" min="20" max="150" bind:value={budget} />
+    <input
+      class="text-right w-16 "
+      id="budget"
+      type="number"
+      bind:value={budget}
+    />
+    <span>DT</span>
+  </div>
+  <div>
+    <label for="date">Indiquer la date : </label>
+    <input id="date" type="date" name="date" />
+  </div>
+
+  <div>
+    <label for="location">Adresse</label>
+    <input class="border" type="text" id="location" name="location" required />
+  </div>
+  <input class="self-center" type="submit" value="Publier" />
+</form>
