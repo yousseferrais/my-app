8306c88017782900d8dbbe8a10e575b777bb84a9
Yoyo <youssef.errais@adaures.com>
2022-09-01 14:46:36 +0100

    feat: add input component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,8 @@
-<nav class="flex justify-around">
-  <a
-    class="border-2 border-yellow-500 rounded-full px-16 py-2 font-bold text-yellow-500 hover:bg-yellow-300 hover:text-white"
-    href="/registration">Inscription</a
-  >
-  <a
-    class="border-2 border-yellow-500 rounded-full px-16 py-2 font-bold text-yellow-500 hover:bg-yellow-300 hover:text-white"
-    href="/sign-in">Connexion</a
-  >
-</nav>
-<main>
-  <ul class="mt-20 px-20 flex flex-col gap-4">
-    <li>Créer un compte</li>
-    <li>Sélectionner le service désiré</li>
-    <li>Publier une annonce</li>
-    <li>Indiquer le budget</li>
-    <li>Recevoir des offres</li>
-    <li>Contacter le prestataire</li>
-  </ul>
-</main>
+<ul class="mt-20 px-20 flex flex-col gap-4">
+  <li>Créer un compte</li>
+  <li>Sélectionner le service désiré</li>
+  <li>Publier une annonce</li>
+  <li>Indiquer le budget</li>
+  <li>Recevoir des offres</li>
+  <li>Contacter le prestataire</li>
+</ul>
--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -1,97 +1,50 @@
+<script lang="ts">
+  import Input from "$components/Input.svelte";
+</script>
+
 <form
-  class="flex w-full  max-w-lg  flex-col mx-auto gap-6 mt-48 bg-slate-600 p-2 rounded-md  "
+  class="grid grid-cols-2 w-full text-xl max-w-2xl  flex-col mx-auto gap-4 mt-48 bg-black bg-opacity-50 p-4 rounded-lg  "
   action="#"
 >
-  <div class="flex gap-3">
-    <div class="flex flex-col gap-1  w-1/2">
-      <label for="firstname">Prénom :</label>
-      <input
-        class="border-2  border-neutral-800 px-2 "
-        type="text"
-        id="firstname"
-        name="ftname"
-        required
-      />
-    </div>
-    <div class="flex flex-col gap-1  w-1/2">
-      <label for="lastname">Nom :</label>
-      <input
-        class="border-2  border-neutral-800 px-2 "
-        type="text"
-        id="lastname"
-        name="lname"
-        required
-      />
-    </div>
-  </div>
-  <div class="flex gap-4  w-1/2">
-    <label for="gender">Je suis : </label>
-    <select name="gender" id="gender">
+  <Input label="Nom :" type="text" name="lastName" flexcol={true} />
+  <Input label="Prénom :" type="text" name="firstName" flexcol={true} />
+  <div class="flex flex-col gap-1 ">
+    <label for="gender" class="text-white">Je suis :</label>
+    <select name="gender" id="gender" class="flex-1">
       <option value="" disabled selected hidden />
       <option value="male">un homme</option>
       <option value="female">une femme</option>
     </select>
   </div>
 
-  <div class="flex  gap-1  w-full">
-    <label for="location">Adresse :</label>
-    <input
-      class="border-2 flex-1 border-neutral-800 px-2 "
-      type="text"
-      id="location"
-      name="location"
-      required
-    />
-  </div>
-
-  <div class="flex gap-3">
-    <div class="flex flex-col gap-1  w-1/2">
-      <label for="phone">Numéro de téléphone :</label>
-      <input
-        class="border-2  border-neutral-800 px-2 "
-        type="text"
-        id="phone"
-        name="phone"
-        pattern="[0-9]{8}"
-        required
-      />
-    </div>
-    <div class="flex flex-col gap-1  w-1/2">
-      <label for="email">E-mail :</label>
-      <input
-        class="border-2  border-neutral-800 px-2 "
-        type="email"
-        id="email"
-        name="email"
-        placeholder="flen@gmail.com"
-        required
-      />
-    </div>
-  </div>
-  <div class="flex gap-3">
-    <div class="flex flex-col gap-1  w-1/2">
-      <label for="mot">Mot de passe :</label>
-      <input
-        class="border-2  border-neutral-800 px-2 "
-        type="password"
-        id="mot"
-        name="mot"
-        required
-      />
-    </div>
-    <div class="flex flex-col gap-1  w-1/2">
-      <label for="confirmer">Confirmer le mot de passe :</label>
-      <input
-        class="border-2  border-neutral-800 px-2 "
-        type="password"
-        id="confirmer"
-        name="confirmer"
-        required
-      />
-    </div>
-  </div>
+  <Input label="Adresse :" type="text" name="location" flexcol={true} />
+  <Input
+    label="E-mail :"
+    type="email"
+    name="email"
+    placeHolder="flen.benfalten@gmail.com"
+    flexcol={true}
+  />
+  <Input
+    label="Numéro de téléphone :"
+    type="text"
+    name="phone"
+    flexcol={true}
+  />
+  <Input
+    label="Mot de passe :"
+    type="password"
+    name="password"
+    flexcol={true}
+  />
+  <Input
+    label="Confirmer le mot de passe :"
+    type="password"
+    name="passwordConfirmation"
+    flexcol={true}
+  />
   <input
-    class="border-2  border-neutral-800 px-5 self-center bg-white "
+    class="border-2 col-span-2 w-max place-self-center border-neutral-800 px-5 self-center bg-white "
     type="submit"
     value="S'inscrire"
   />
